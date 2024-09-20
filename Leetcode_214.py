4cef1c764e11d626a38bcadab7601d35e290bc24
Aditya Singh <75111366+AdityaSingh12@users.noreply.github.com>
2024-09-20 22:40:10 +0530

    Add files via upload

---
+++ Leetcode_214.py
@@ -0,0 +1,15 @@
+"""
+Shortest Palindrome
+
+You are given a string s.
+You can convert s to a palindrome by adding characters in front of it.
+
+Return the shortest palindrome you can find by performing this transformation.
+"""
+
+class Solution:
+    def shortestPalindrome(self, s: str) -> str:
+        r = s[::-1]
+        for i in range(len(s) + 1):
+            if s.startswith(r[i:]):
+                return r[:i] + s 
